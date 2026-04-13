aa59521d6a292446409a2d85943a0e59869424a8
s1140535-code <s1140535@dmhs.kh.edu.tw>
2026-04-13 10:03:46 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {
+	int age = 17;
+    string name ="謝函芸是傻逼";
+	cout << "年齡:" << age << "姓名:" << name <<endl;
+	return 0;
+}
