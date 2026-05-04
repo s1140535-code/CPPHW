c7111b3fdadeaaf19e34c06cc9a021cd4f2b6969
s1140535-code <s1140535@dmhs.kh.edu.tw>
2026-05-04 09:50:18 +0800

    Create  logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int b = 3;
+    int c = a - b;
+    cout << (a < b) << " ";
+    cout << (a >= b) << " ";
+    cout << (a >= c) << " ";
+    cout <<  (b > c) << " ";
+    cout << (a == b) << " ";
+    cout << (a > b) << " ";
+    cout << (a != b) << " ";
+
+    return 0;
+}
